2ca40014d9a244eeda82a82197288d295ce20db9
Deyvis Valdez <deyvis.vh@gmail.com>
2017-10-05 23:17:22 -0500

    favorite button and debug bar

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,9 +1,22 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a>
-        said {{ $reply->created_at->diffForHumans() }}
+        <div class="level">
+            <div class="flex">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a> said {{ $reply->created_at->diffForHumans() }}...
+            </div>
+
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    {{ csrf_field() }}
+                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                    {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
+                    </button>
+                </form>
+            </div>
+        </div>
+
     </div>
 
     <div class="panel-body">
