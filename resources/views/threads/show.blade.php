5192aa1cfcd1aa663065bead6fed657aa76dac5a
Deyvis Valdez <deyvis.vh@gmail.com>
2017-10-12 23:09:15 -0500

    user policies - owner update

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -9,17 +9,17 @@
                     <div class="level">
                         <span class="flex">
                             <a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted:
-                            {{ $thread->title }}
+                            <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
                         </span>
 
-                        @if (Auth::check())
+                        @can ('update', $thread)
                             <form action="{{ $thread->path() }}" method="POST">
                                 {{ csrf_field() }}
                                 {{ method_field('DELETE') }}
 
                                 <button type="submit" class="btn btn-link">Delete Thread</button>
                             </form>
-                        @endif
+                        @endcan
                     </div>
                 </div>
 
